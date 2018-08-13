04612e3e1e7bf0dc121a5610e1f1fbac6b0077f4
Jeff Barnhart <jeffreycbarnhart@gmail.com>
2018-08-13 19:57:03 -0400

    initial commit

---
+++ src/main/java/com/ww101/gnarbot/GnarbotApplication.java
@@ -0,0 +1,12 @@
+package com.ww101.gnarbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnarbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnarbotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ww101/gnarbot/GnarbotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ww101.gnarbot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GnarbotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
